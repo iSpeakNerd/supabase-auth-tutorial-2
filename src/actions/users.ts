be19e04e9418f47c5f3ab7daa772206fbfa659f9
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2025-06-17 12:59:43 -0700

    signout

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,53 +1,58 @@
-'use server'
+'use server';
 
-import { createSupabaseClient } from "@/auth/server"
-import { getErrorMessage } from "@/lib/utils"
+import { createSupabaseClient } from '@/auth/server';
+import { getErrorMessage } from '@/lib/utils';
 
 export async function createAccountAction(formData: FormData) {
-    try {
-        const email = formData.get('email') as string
-        const password = formData.get('password') as string
+  try {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    // ! does not validate email and password before passing to client, beware!
 
-        const {auth} = await createSupabaseClient()
+    const { auth } = await createSupabaseClient();
 
-        const {error} = await auth.signUp({email, password})
+    const { error } = await auth.signUp({ email, password });
 
-        if (error) {
-            throw error
-        }
-        return {errorMessage: null}
-    } catch (error) {
-        return {errorMessage: getErrorMessage(error)}
+    if (error) {
+      throw error;
     }
+    return { errorMessage: null };
+  } catch (error) {
+    return { errorMessage: getErrorMessage(error) };
+  }
 }
 
 export async function loginAction(formData: FormData) {
-    try {
-        const email = formData.get('email') as string
-        const password = formData.get('password') as string
+  try {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
 
-        const {auth} = await createSupabaseClient()
+    const { auth } = await createSupabaseClient();
 
-        const {error} = await auth.signInWithPassword({email, password})
-        if (error) {
-            throw error
-        }
-        return {errorMessage: null}
-    } catch (error) {
-        return {errorMessage: getErrorMessage(error)}
+    const { data, error } = await auth.signInWithPassword({ email, password });
+    const jwt = data.session?.access_token;
+
+    console.debug('user access_token = ', jwt);
+
+    if (error) {
+      throw error;
     }
+    return { errorMessage: null };
+  } catch (error) {
+    return { errorMessage: getErrorMessage(error) };
+  }
 }
 
 export async function signOutAction() {
-    try {
-        const {auth} = await createSupabaseClient()
+  try {
+    const { auth } = await createSupabaseClient();
 
-        const {error} = await auth.signOut()
-        if (error) {
-            throw error
-        }
-        return {errorMessage: null}
-    } catch (error) {
-        return {errorMessage: getErrorMessage(error)}
+    const { error } = await auth.signOut();
+    if (error) {
+      throw error;
     }
+    return { errorMessage: null };
+  } catch (error) {
+    return { errorMessage: getErrorMessage(error) };
+  }
 }
--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,35 +1,36 @@
 "use client";
 
-import { createSupabaseClient } from '@/auth/server';
-import { getErrorMessage } from '@/lib/utils';
-import {useRouter} from 'next/navigation'
-import { useTransition } from 'react';
-import toast from 'react-hot-toast';
-import {signOutAction} from '@/actions/users'
-import { Loader2 } from 'lucide-react';
+import { createSupabaseClient } from "@/auth/server";
+import { getErrorMessage } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import toast from "react-hot-toast";
+import { signOutAction } from "@/actions/users";
+import { Loader2 } from "lucide-react";
 
 function SignOutButton() {
   const router = useRouter();
-  const [isPending, startTransition] = useTransition()
+  const [isPending, startTransition] = useTransition();
 
   const handleClickSignOutButton = async () => {
-    startTransition(async ()=> {
-      const {errorMessage} = await signOutAction()
-    if (errorMessage) {
-      toast.error(errorMessage)
-    } else {
-      router.push('/')
-      toast.success('Successfully logged out.')
-    }})
-  }
-    
+    startTransition(async () => {
+      const { errorMessage } = await signOutAction();
+      if (errorMessage) {
+        toast.error(errorMessage);
+      } else {
+        router.push("/");
+        toast.success("Successfully logged out.");
+      }
+    });
+  };
+
   return (
     <button
       onClick={handleClickSignOutButton}
       className="rounded-lg p-2 text-white flex justify-center bg-emerald-700 w-40"
       disabled={isPending}
     >
-      {isPending ? <Loader2 className='animate-spin'/> : 'Sign Out'}
+      {isPending ? <Loader2 className="animate-spin" /> : "Sign Out"}
     </button>
   );
 }
